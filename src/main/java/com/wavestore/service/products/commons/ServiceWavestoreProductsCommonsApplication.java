5a23e5681081d966f6c542fc23d6a8750e067907
starwalker <juan.rodriguez2803@hotmail.com>
2023-04-02 22:22:23 -0600

    enable auto config

--- src/main/java/com/wavestore/service/products/commons/ServiceWavestoreProductsCommonsApplication.java
+++ src/main/java/com/wavestore/service/products/commons/ServiceWavestoreProductsCommonsApplication.java
@@ -1,13 +1,11 @@
 package com.wavestore.service.products.commons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class ServiceWavestoreProductsCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceWavestoreProductsCommonsApplication.class, args);
-	}
 
 }
